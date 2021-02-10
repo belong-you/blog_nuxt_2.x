4052c4b646a63f3c2ea2ff91c5743912bdc2d17a
belong-you <1781926993@qq.com>
2021-02-10 22:25:51 +0800

    blog 2.x

---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+	<div>
+		<Header />
+		<Nuxt />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+	components: {
+		Header,
+		Footer,
+	},
+}
+</script>
+
+<style>
+body, ul, h1, h2, h3, h4, h5, h6, p{
+	margin: 0;
+}
+ul, input{
+	padding: 0;
+}
+.layer{
+	margin: 60px auto;
+	max-width: 1200px;
+}
+ul{
+	list-style: none;
+}
+a{
+	text-decoration: none;
+	color: #003bfc;
+}
+input{
+	border: 1px solid;
+	outline : none;
+}
+.clearfix::after{
+	content: '';
+	display: block;
+	clear: both;
+}
+.fl{
+	float: left;
+}
+.fr{
+	float: right;
+}
+
+</style>
