917cc962bfa5913c042c1b9858c211433452b3ea
belong-you <1781926993@qq.com>
2021-02-10 22:24:06 +0800

    首页

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="container">hello
+
+	</div>
+</template>
+
+<script>
+export default {
+	provide () {
+		return {
+			getLabel: () => 123
+		}
+	}
+}
+</script>
+
+<style>
+</style>
